1a82079fa10cd3bc9c32e6690796b2ff63991540
ViraMesha <wira.mescha@gmail.com>
2023-10-06 21:30:50 +0300

    add markup and styles for mobile

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", documentClick);
+
+function documentClick(event) {
+  const targetItem = event.target;
+
+  if (targetItem.closest(".icon-menu")) {
+    document.documentElement.classList.toggle("menu-open");
+  }
+}
